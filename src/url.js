27f515e021e55530f19d1ed6aa0fc1b4eb4c0b72
oyejohnson <johnsonoye34@gmail.com>
2018-12-19 23:26:05 +0100

    changed the url error

--- src/url.js
+++ src/url.js
@@ -1,5 +1,5 @@
 // const url = "https://rifqoe.herokuapp.com";
-const url = "https://vinepay-api.herokuapp.com/";
+const url = "https://vinepay-api.herokuapp.com";
 const token = localStorage.getItem("token");
 export default url;
 export { token };

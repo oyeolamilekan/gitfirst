c87fa6ec74ca5a18bbea5cd2492ba220802b46cf
oyejohnson <johnsonoye34@gmail.com>
2018-12-20 14:00:22 +0100

    paysquared

--- src/url.js
+++ src/url.js
@@ -1,5 +1,5 @@
-// const url = "https://vinepay-api.herokuapp.com";
-const url = "http://127.0.0.1:8000";
+const url = "https://vinepay-api.herokuapp.com";
+// const url = "http://127.0.0.1:8000";
 const token = localStorage.getItem("token");
 export default url;
 export { token };
